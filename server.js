b05ea85fdef40dacd07bc684d122d0388addae10
JEEVANA SANTHOSH MANIKANTA REDDY <jsmanikanta13@gmail.com>
2026-03-23 11:12:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -59,6 +59,6 @@ app.get("/", (req, res) => {
 
 // Start server
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on port ${port}`);
 });
